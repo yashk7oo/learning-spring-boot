9ee2b4bb745956f5b317d4700a9b190d4025c80d
Yash Kumar <yash.kumar@nike.com>
2023-10-19 02:23:14 +0530

    devtools dependency added and tested

--- src/main/java/com/demo/example/learning/controller/HelloWorldController.java
+++ src/main/java/com/demo/example/learning/controller/HelloWorldController.java
@@ -1,20 +1,30 @@
 package com.demo.example.learning.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${name}")
+    private String userName;
+
     @GetMapping("/")
     public String getHelloWorld() {
-        return "Hello World";
+        return ("Hello World "+userName);
     }
 
     @GetMapping("/awesome")
     public String getAwesome() {
         return "Awesome";
     }
+
+    @GetMapping("/getName")
+    public String getName() {
+        return userName;
+    }
+    
     
     
 }
